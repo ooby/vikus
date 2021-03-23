f00b6efbbb030dade8da09bdce880bb744ddcbe8
Afanasy Argunov <afanasy@argunov.com>
2021-03-23 15:44:38 +0900

    study window, level adjust acceleration, text overlay to pixmap, study list backgrounds, export study

--- libs/widgets.py
+++ libs/widgets.py
@@ -1,7 +1,7 @@
 import numpy as np
-from PyQt5.QtCore import QEvent, QSize, Qt
-from PyQt5.QtGui import QImage, QPixmap, QWheelEvent
-from PyQt5.QtWidgets import QHeaderView, QLabel, QSizePolicy, QTableWidget
+from PyQt5.QtCore import QEvent, QPoint, QSize, Qt
+from PyQt5.QtGui import QBrush, QColor, QFont, QFontDatabase, QImage, QPainter, QPalette, QPixmap, QWheelEvent
+from PyQt5.QtWidgets import QAbstractItemView, QHeaderView, QLabel, QSizePolicy, QTableWidget
 from .import_files import get_pixels, windowed_rgb
 
 
@@ -10,6 +10,8 @@ class DicomList(QTableWidget):
         super(DicomList, self).__init__(*args, **kwargs)
         self.studies_list = studies_list
         self.express_view = express_view
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
         self.setColumnCount(7)
         self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.setHorizontalHeaderLabels([
@@ -17,16 +19,17 @@ class DicomList(QTableWidget):
             "Date acquired", "Time acquired"
         ])
         self.viewport().installEventFilter(self)
+        _id = QFontDatabase.addApplicationFont("fonts/Rokkitt-Light.ttf")
 
     def eventFilter(self, source, event):
         if (event.type() == QEvent.MouseButtonPress and
                 event.buttons() == Qt.LeftButton and source is self.viewport()):
             item = self.itemAt(event.pos())
             if item is not None:
-                study = self.studies_list[int(item.row())]["data"][0]
-                pixels = get_pixels(study)
+                self.study_data = self.studies_list[int(item.row())]["data"][0]
+                pixels = get_pixels(self.study_data)
                 DicomExpressView.updateConvertPixmap(
-                    self.express_view, pixels, 0)
+                    self.express_view, self.study_data, pixels, 0)
         return super(DicomList, self).eventFilter(source, event)
 
 
@@ -41,11 +44,22 @@ class DicomExpressView(QLabel):
         self.pixmap = QPixmap(qimage)
         self.installEventFilter(self)
         self.setPixmap(self.pixmap)
-    
+
     def mousePressEvent(self, event):
         if hasattr(self, 'pixels') and len(self.pixels) > 0:
             self.prev_x = event.pos().x()
             self.prev_y = event.pos().y()
+        return super(DicomExpressView, self).mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if hasattr(self, 'pixels') and len(self.pixels) > 0:
+            min_hu = self.level - self.window // 2
+            max_hu = self.level + self.window // 2
+            pixels = self.pixels
+            pixels = np.where(pixels < min_hu, min_hu, pixels)
+            pixels = np.where(pixels > max_hu, max_hu, pixels)
+            self.rgb_pixels = windowed_rgb(pixels, min_hu, max_hu)
+            self.updatePixmap(self.position)
         return super(DicomExpressView, self).mousePressEvent(event)
 
     def mouseMoveEvent(self, event):
@@ -57,14 +71,17 @@ class DicomExpressView(QLabel):
                 curr_y = event.pos().y()
                 y_diff = curr_y - self.prev_y
                 self.prev_y = curr_y
-                self.window += 2 * x_diff
-                self.level += y_diff                
+                self.window += x_diff
+                if self.window < 0:
+                    self.window = 0
+                self.level -= y_diff
                 # print(self.window, self.level)
         min_hu = self.level - self.window // 2
         max_hu = self.level + self.window // 2
-        pixels = np.where(self.pixels < min_hu, min_hu, self.pixels)
+        pixels = self.pixels[self.position]
+        pixels = np.where(pixels < min_hu, min_hu, pixels)
         pixels = np.where(pixels > max_hu, max_hu, pixels)
-        self.rgb_pixels = windowed_rgb(pixels, min_hu, max_hu)
+        self.rgb_pixels[self.position] = windowed_rgb(pixels, min_hu, max_hu)
         self.updatePixmap(self.position)
         return super(DicomExpressView, self).mouseMoveEvent(event)
 
@@ -83,12 +100,11 @@ class DicomExpressView(QLabel):
 
     def updatePixmap(self, position):
         pixels_to_set = self.rgb_pixels[position]
-        qimage = QImage(pixels_to_set, pixels_to_set.shape[1],
-                        pixels_to_set.shape[0], QImage.Format_RGB888)
-        self.pixmap = QPixmap(qimage)
+        self.createPixmap(pixels_to_set)
         self.setPixmap(self.pixmap.scaled(self.size(), Qt.KeepAspectRatio))
 
-    def updateConvertPixmap(self, image, position):
+    def updateConvertPixmap(self, study, image, position):
+        self.study_data = study
         self.pixels = image
         self.window = abs(np.max(self.pixels) - np.min(self.pixels))
         self.level = np.max(self.pixels) - (self.window // 2)
@@ -96,12 +112,39 @@ class DicomExpressView(QLabel):
         self.rgb_pixels = windowed_rgb(image)
         self.position = position
         pixels_to_set = self.rgb_pixels[position]
-        qimage = QImage(pixels_to_set, pixels_to_set.shape[1],
-                        pixels_to_set.shape[0], QImage.Format_RGB888)
-        self.pixmap = QPixmap(qimage)
+        self.createPixmap(pixels_to_set)
         self.setPixmap(self.pixmap.scaled(self.size(), Qt.KeepAspectRatio))
 
     def eventFilter(self, source, event):
         if (source is self and event.type() == QEvent.Resize):
             self.setPixmap(self.pixmap.scaled(self.size(), Qt.KeepAspectRatio))
         return super(DicomExpressView, self).eventFilter(source, event)
+
+    def createPixmap(self, pixels):
+        qimage = QImage(
+            pixels, pixels.shape[1], pixels.shape[0], QImage.Format_RGB888)
+        self.pixmap = QPixmap(qimage)
+        qp = QPainter()
+        qp.begin(self.pixmap)
+        qp.setRenderHint(QPainter.TextAntialiasing)
+        font = QFont()
+        font.setFamily("Rokkitt")
+        font.setBold(False)
+        font.setPointSize(10)
+        qp.setFont(font)
+        qp.setPen(QColor(255, 255, 255))
+        position = QPoint(5, 15)
+        qp.drawText(position, f"Image size: {pixels.shape[0]}x{pixels.shape[1]}")
+        position = QPoint(5, 26)
+        qp.drawText(position, f"View size: {self.size().width()}x{self.size().width()}")
+        position = QPoint(5, 37)
+        qp.drawText(position, f"WL: {self.level} WW: {self.window}")
+        position = QPoint(5, 48)
+        qp.drawText(position, f"Image: {self.position}/{len(self.pixels)}")
+        position = QPoint(5, 478)
+        qp.drawText(position, f"{str(self.study_data[0].SeriesDescription).strip()}")
+        position = QPoint(5, 489)
+        qp.drawText(position, f"{str(self.study_data[0].StudyDescription).strip()}")
+        position = QPoint(5, 500)
+        qp.drawText(position, f"{str(self.study_data[0].PatientName).strip()}")
+        qp.end()
